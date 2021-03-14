342c59d64a9d49a2535926e935d2106d2e0ac6e9
Noah <noahschnur@gmail.com>
2021-03-14 14:05:51 -0700

    localStorage and current weather working, post tutor

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,55 +3,73 @@ var currentWetherContainerEl = $("#current-weather-container");
 var fiveDayForcastContainerEl = $("#five-day-focast-container");
 
 // Click on submit button
-$("#weather").click(function(event) {
-    // prevent page from refreshing
-    event.preventDefault();
-    event.stopPropagation();
-    
-    var cityName = $("#city").val().trim();
-        showHistory.push(cityName);
-    // console.log(showHistory)
-    cityWeather(cityName);
-    localStorage.setItem("cities", JSON.stringify(showHistory));
+$("#weather").click(function (event) {
+  // prevent page from refreshing
+  event.preventDefault();
+
+  // Click save for localStorage
+  var cityName = $("#city").val().trim();
+  showHistory.push(cityName);
+  // console.log(showHistory)
+  localStorage.setItem("cities", JSON.stringify(showHistory));
+  cityWeather(cityName);
 });
 
+// get from local storage
+if (localStorage.getItem("cities")) {
+  JSON.parse(localStorage.getItem("cities")).map((city) =>
+    showHistory.push(city)
+  );
+  cityWeather(showHistory[showHistory.length - 1]);
+}
+
 // Call API for current weather
-var cityWeather = function(city) {
-    // console.log(city);
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${4eac509137831fda29e6f441e0ef2afe}")
-        .then(function(response) {
-            return response.JSON();
-        })
-            .then(function(response) {
-                var currentWeather = currentWetherContainerEl;
-                // Set elements 'src' attribute to the NEEDINFO from API response                currentWeather.setAttribute('src', response.data.NEEDINFO);
-                // Append the current weather element to the page
-                currentWetherContainerEl.appendChild(currentWeather);
-            });
-};
-
-// Call API for 5 day forcast
-var futureWeather = function(city) {
-    // consol.log(city);
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q={cityName}&appid=${1a70f58ef96d2a0e1cdc65e35763d464}")
-    .then(function(response) {
-        return response.JSON();
+function cityWeather(cityName) {
+  for (let i = 0; i < showHistory.length; i++) {
+    var historyLi = $("<li>");
+    historyLi.text(showHistory[i]);
+    $("#show-history").append(historyLi);
+  }
+  //    console.log(cityName);
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=4eac509137831fda29e6f441e0ef2afe`
+  )
+    .then(function (response) {
+      return response.json();
     })
-        .then(function(response) {
-            var fiveDay = fiveDayForcastContainerEl;
-            // Set elements 'src' attribute to the NEEDINFO from API response
-            fiveDay.setAttribute('src', response.data.NEEDINFO);
-            // Append future weather element to the page
-            fiveDayForcastContainerEl.appendChild(fiveDay);
-        });
-;}
-
-    
-
-
+    .then(function (response) {
+      //   console.log(response);
+      //   var icon = response.weather
+      $("#icon").attr(
+        "src",
+        "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png"
+      );
+      var wind = response.wind.speed;
+      $("#wind-speed").text(wind);
+      var temp = response.main.temp;
+      $("#temp").text(temp);
+      var humid = response.main.humidity;
+      $("#humid").text(humid);
+    });
+}
+// Call API for 5 day forcast
+function futureWeather(cityName) {
+  consol.log(cityName);
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=1a70f58ef96d2a0e1cdc65e35763d464`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      $("#five-days").attr(
+        "src",
+        "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png"
+      );
+    });
+}
 
 // 5 day api key: 1a70f58ef96d2a0e1cdc65e35763d464
 // api 5 day forcast: api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
 // api current weather: api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
 //  current api key: 4eac509137831fda29e6f441e0ef2afe
-        
