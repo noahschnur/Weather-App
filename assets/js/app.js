385732c35412dd28139679f7e8224bfe2fd5e991
Noah <noahschnur@gmail.com>
2021-03-14 22:44:14 -0700

    final

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,10 @@ var showHistory = [];
 var currentWetherContainerEl = $("#current-weather-container");
 var fiveDayForcastContainerEl = $("#five-day-focast-container");
 
+// Add current day to p
+var currentDay = moment().format("MMM Do YYYY");
+    $("#current-day").text(currentDay);
+
 // Click on submit button
 $("#weather").click(function (event) {
   // prevent page from refreshing
@@ -30,7 +34,7 @@ function cityWeather(cityName) {
     historyLi.text(showHistory[i]);
     $("#show-history").append(historyLi);
   }
-  //    console.log(cityName);
+  // console.log(cityName);
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=4eac509137831fda29e6f441e0ef2afe`
   )
@@ -44,6 +48,7 @@ function cityWeather(cityName) {
         "src",
         "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png"
       );
+      $("#city-name").text(cityName);
       var wind = response.wind.speed;
       $("#wind-speed").text(wind);
       var temp = response.main.temp;
@@ -68,6 +73,14 @@ function futureWeather(cityName) {
       );
     });
 }
+
+// function ReSearch() {
+//     document.getElementById("show-history").click();
+//   }
+
+// var reSearch = $("#show-history");
+// reSearch.addEventListener('click', cityWeather)
+
 
 // 5 day api key: 1a70f58ef96d2a0e1cdc65e35763d464
 // api 5 day forcast: api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
